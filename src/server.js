c9efca9c083bb3fbbf9fd23ab1567be0bb2739c5
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2020-01-31 11:20:30 -0500

    initial commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+// Catch-all 404
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Catch-all Error handler
+// Add NODE_ENV check to prevent stacktrace leak
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
+
+app.listen(8080,()=>{
+  console.log('Serving on 8080');
+});
