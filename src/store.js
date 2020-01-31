cad87bf965ef57364fb88a0f327a3dfed73dd2c7
AustinT33 <austintumlinson@gmail.com>
2020-01-31 12:33:20 -0600

    building queue stack

--- src/store.js
+++ src/store.js
@@ -1,14 +1,140 @@
-'use strict';
 const STORE = {
+    dogs: [
+      {
+        id: 1,
+        imageURL:
+          'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+        imageDescription:
+          'A smiling golden-brown golden retreiver listening to music.',
+        user_name: 'Thinkful',
+        name: 'Zeus',
+        sex: 'Male',
+        age: 3,
+        breed: 'Golden Retriever',
+        story: 'Return by previous owner'
+      },
+      {
+        id: 2,
+        imageURL:
+          'https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        imageDescription:
+          'A smiling white fluffy dog enjoying the grass.',
+        user_name: 'Nick',
+        name: 'Hera',
+        sex: 'Female',
+        age: 5,
+        breed: 'German Shephard',
+        story: 'Owner Passed away'
+      },
+      {
+        id: 3,
+        imageURL:
+          'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        imageDescription:
+          'a close up of this energetic pup.',
+        user_name: 'Rahim',
+        name: 'Hercules',
+        sex: 'Male',
+        age: 15,
+        breed: 'Chow Chow',
+        story: 'Run away'
+      },
+      {
+        id: 4,
+        imageURL:
+          'https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        imageDescription:
+          'A smiling golden-brown golden retreiver taking a stroll in the forest.',
+        user_name: 'Peter',
+        name: 'Chord',
+        sex: 'Male',
+        age: 1,
+        breed: 'Bichon',
+        story: 'Puppy'
+      },
+      {
+        id: 5,
+        imageURL:
+          'https://images.pexels.com/photos/58997/pexels-photo-58997.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        imageDescription:
+          'A very nice corgi.',
+        user_name: 'John',
+        name: 'Max',
+        sex: 'Male',
+        age: 3,
+        breed: 'Pitbull',
+        story: 'Caught'
+      }
+    ],
+  
     cats: [
-        {
-        imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-        imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-        name: 'Fluffy',
+      {
+        id: 1,
+        imageURL:
+          'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+        imageDescription:
+          'Orange bengal cat with black stripes lounging on concrete.',
+        user_name: 'Thinkful',
+        name: 'Stripe',
+        sex: 'Female',
+        age: 2,
+        breed: 'Tabby',
+        story: 'Thrown on the street'
+      },
+      {
+        id: 2,
+        imageURL:
+          'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+        imageDescription:
+          'white and gray cat with amber eyes.',
+        user_name: 'Nick',
+        name: 'Scar',
+        sex: 'Female',
+        age: 2,
+        breed: 'Mixed',
+        story: 'Sauntered up'
+      },
+      {
+        id: 3,
+        imageURL:
+          'https://images.pexels.com/photos/730896/pexels-photo-730896.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        imageDescription:
+          'Gray fluffy cat with brown eyes.',
+        user_name: 'Rahim',
+        name: 'Boss',
         sex: 'Female',
         age: 2,
         breed: 'Bengal',
         story: 'Thrown on the street'
-        },
-    ]
-}
+      },
+      {
+        id: 4,
+        imageURL:
+          'https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+        imageDescription:
+          'white Cat with brown spots',
+        user_name: 'Peter',
+        name: 'Ben',
+        sex: 'Male',
+        age: 1,
+        breed: 'Black Cat',
+        story: 'Abandoned'
+      },
+      {
+        id: 5,
+        imageURL:
+          'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        imageDescription:
+          'green eye striped cat',
+        user_name: 'John',
+        name: 'Fluffy',
+        sex: 'Female',
+        age: 5,
+        breed: 'Mixed',
+        story: 'Injured'
+      }
+    ],
+
+};
+
+module.exports = STORE;
